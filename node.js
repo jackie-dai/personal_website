594e70092c6dbfafcbe41f1319699272ec34bc8a
jackiedai <jackie123768@gmail.com>
2023-10-01 14:26:36 -0700

    Add node

---
+++ node.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
